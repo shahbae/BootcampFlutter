796d75c3830fe46e6c483e4e517632c1aec872b6
shahbae <alamelreal@gmail.com>
2024-01-16 14:22:51 +0700

    tugas hari 2

---
+++ Tugas 2A–TypeData/type.dart
@@ -0,0 +1,62 @@
+import 'dart:io';
+
+void main() {
+  // //NO 1
+  // // =================
+  // var word = 'dart';
+  // var second = 'is';
+  // var third = 'awesome';
+  // var fourth = 'and';
+  // var fifth = 'I';
+  // var sixth = 'love';
+  // var seventh = 'it!';
+
+  // var output = '$word $second $third $fourth $fifth $sixth $seventh';
+  // print(output);
+
+  // //NO 2
+  // // =================
+  // var sentence = "I am going to be Flutter Developer";
+
+  // var exampleFirstWord = sentence.substring(0, 1);
+  // var exampleSecondWord = sentence.substring(2, 4);
+  // var thirdWord = sentence.substring(5, 10);
+  // var fourthWord = sentence.substring(11, 13);
+  // var fifthWord = sentence.substring(14, 16);
+  // var sixthWord = sentence.substring(17, 23);
+  // var seventhWord = sentence.substring(24);
+
+  // print('First Word: ' + exampleFirstWord);
+  // print('Second Word: ' + exampleSecondWord);
+  // print('Third Word: ' + thirdWord);
+  // print('Fourth Word: ' + fourthWord);
+  // print('Fifth Word: ' + fifthWord);
+  // print('Sixth Word: ' + sixthWord);
+  // print('Seventh Word: ' + seventhWord);
+
+  //NO 3
+  // =================
+  // print("masukan nama depan :");
+  // String? namaDepan = stdin.readLineSync()!;
+  // print("masukan nama belakang :");
+  // String? namaBelakang = stdin.readLineSync()!;
+  // print("nama lengkap anda adalah:");
+  // print("${namaDepan} ${namaBelakang}");
+
+  //NO 4
+  // =================
+  // final a = 5;
+  // final b = 10;
+
+  // final perkalian = a * b;
+  // print("perkalian : ${perkalian} ");
+
+  // final pembagian = a / b;
+  // print("pembagian : ${pembagian} ");
+
+  // final penambahan = a + b;
+  // print("penambahan : ${penambahan} ");
+
+  // final pengurangan = a - b;
+  // print("pengurangan : ${pengurangan} ");
+}
