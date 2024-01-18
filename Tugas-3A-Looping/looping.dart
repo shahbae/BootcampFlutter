f70c83c87042414bfc2a3e125b861785f166649e
shahbae <alamelreal@gmail.com>
2024-01-18 10:21:12 +0700

    tugas hari 3

---
+++ Tugas-3A-Looping/looping.dart
@@ -0,0 +1,44 @@
+void main() {
+  // //NO 1 Looping While
+  // //================
+  // print("LOOPING PERTAMA");
+  // int i = 2;
+  // while (i <= 20) {
+  //   print("$i - I love coding");
+  //   i += 2;
+  // }
+
+  // print("LOOPING KEDUA");
+  // int j = 20;
+  // while (j >= 2) {
+  //   print("$j - I will become a mobile developer");
+  //   j -= 2;
+  // }
+
+  // //NO 2 Looping menggunakan for
+  // //================
+  // for (int i = 1; i <= 20; i++) {
+  //   if (i % 2 == 0) {
+  //     print("$i - Berkualitas");
+  //   } else {
+  //     print("$i - Santai");
+  //   }
+
+  //   if (i % 3 == 0 && i % 2 == 1) {
+  //     print("$i - I Love Coding");
+  //   }
+  // }
+
+  //NO 3 Membuat Persegi Panjang #
+  //===============
+
+    //  for(int i=0;i<=4;i++){
+    //     print("#" * 8);
+    //  }  
+
+  //NO4 Membuat Tangga
+  //===============
+    //  for(int i=0;i<=7;i++){
+    //     print("#" * i);
+    //  }  
+}
---
+++ Tugas-3B-Function/function.dart
@@ -0,0 +1,68 @@
+// //NO 1 
+// //================
+
+// String teriak() {
+//   return "Halo Sanbers!";
+// }
+
+// void main() {
+//   print(teriak());
+// }
+
+
+// //NO 2
+// //================
+
+// int kalikan(int angka1, int angka2) {
+//   return angka1 * angka2;
+// }
+
+// void main() {
+//   var num1 = 12;
+//   var num2 = 4;
+
+//   var hasilKali = kalikan(num1, num2);
+//   print(hasilKali);
+// }
+
+
+// //NO 3
+// //================
+
+// String introduce(String name, int age, String address, String hobby) {
+//   return 'Nama saya $name, umur saya $age tahun, alamat saya di $address, dan saya punya hobby yaitu $hobby!';
+// }
+
+// void main() {
+//   final String name = "Nur Shah Alam";
+//   final int age = 23;
+//   final String address = "Jln. Malioboro, Yogyakarta";
+//   final String hobby = "UI Design";
+
+//   final perkenalan = introduce(name, age, address, hobby);
+//   print(
+//       perkenalan);
+// }
+
+
+// //NO 4 
+// //================
+
+// int faktorial(int angka) {
+//   if (angka <= 0) {
+//     return 1;
+//   } else {
+//     int hasil = 1;
+//     for (int i = angka; i > 0; i--) {
+//       hasil *= i;
+//     }
+//     return hasil;
+//   }
+// }
+
+// void main() {
+//   int angka = 6;
+//   int hasilFaktorial = faktorial(angka);
+
+//   print('$angka! = $hasilFaktorial');
+// }
